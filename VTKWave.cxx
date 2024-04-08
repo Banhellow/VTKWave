32ba7c64cb8209ae88559c13f40d3ff06b3bd878
Banhellow <vladlipinskii2@gmail.com>
2024-04-08 21:52:51 +0200

    Add new way of updating mesh + height based coloring

--- VTKWave.cxx
+++ VTKWave.cxx
@@ -19,10 +19,14 @@
 #include <vtkPoints.h>
 #include <vtkCell.h>
 #include <QPushButton>
+#include <vtkLookupTable.h>
+#include <vtkFloatArray.h>
+#include <vtkPointData.h>
 struct UpdateData {
 	double time;
 	std::chrono::system_clock::time_point startTime;
 	vtkSmartPointer<vtkPolyData> plane_data;
+	vtkSmartPointer<vtkPolyDataMapper> mapper;
 };
 
 vtkSmartPointer <vtkActor> CreateSphereActor(int posX, int posY, int posZ) {
@@ -56,20 +60,38 @@ void UpdateAppTime(double* time, std::chrono::system_clock::time_point start)
 
 }
 
+vtkSmartPointer<vtkLookupTable> CreateLookupTable(float hueMin, float hueMax, float satMin, float satMax, float valMin, float valMax)
+{
+	vtkSmartPointer<vtkLookupTable> lookupTable = vtkSmartPointer<vtkLookupTable>::New();
+	lookupTable->SetRange(-1, 1);
+	lookupTable->SetHueRange(hueMin, hueMax);
+	lookupTable->SetSaturationRange(satMin, satMax);
+	lookupTable->SetValueRange(valMin, valMax);
+	lookupTable->Build();
+	return lookupTable;
+}
+
 void Update(vtkObject* caller, long unsigned int eventId, void* clientData, void* callData)
 {
 	UpdateData* data = (UpdateData*)clientData;
 	vtkRenderWindowInteractor* interactor = static_cast<vtkRenderWindowInteractor*>(caller);
+	vtkSmartPointer<vtkFloatArray> scalars = vtkSmartPointer<vtkFloatArray>::New();
 	vtkSmartPointer<vtkPoints> points = data->plane_data->GetPoints();
+	vtkSmartPointer<vtkLookupTable> lookupTable = CreateLookupTable(0.33, 0.33, 0.6, 0.6, 0.4, 0.8);
 	int size = points->GetNumberOfPoints();
 	double position[3];
 	for (int i = 0; i < size; i++)
 	{
 		double* point = points->GetPoint(i);
-		double zPos = cos(data->time + point[1]);
+		double zPos = 0.5 * cos(data->time + point[0]) + 0.5 * sin(data->time + point[1]);
+		scalars->InsertNextTuple1(zPos);
 		points->SetPoint(i, point[0], point[1], zPos);
 	}
 	points->Modified();
+	data->plane_data->GetPointData()->SetScalars(scalars);
+	data->mapper->SetLookupTable(lookupTable);
+	data->mapper->SetScalarRange(-1, 1);
+	data->mapper->SetScalarVisibility(true);
 	std::cout << data->time << std::endl;
 	UpdateAppTime(&data->time, data->startTime);
 	interactor->GetRenderWindow()->Render();
@@ -148,6 +170,7 @@ int main(int argc, char* argv[])
 	UpdateData clientData;
 	clientData.plane_data = poly_data;
 	clientData.startTime = std::chrono::system_clock::now();
+	clientData.mapper = mapper;
 	clientData.time = 0;
 
 	vtkSmartPointer<vtkCallbackCommand> renderCallback = vtkSmartPointer<vtkCallbackCommand>::New();
